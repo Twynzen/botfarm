a17db9c554fb3db1f9b8cd12f6a67c5b3719bc06
twynzen <razzziel001@gmail.com>
2024-04-10 19:57:51 -0500

    update change map and detect combat

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import pytesseract
+import pandas as pd
 from PIL import Image
 import pyautogui as pg
 import time
 import os
 from PIL import ImageChops 
+import re
 
 # Configura la ubicación de Tesseract en tu sistema
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -17,16 +19,20 @@ trigoPaths = ["ojoIA/trigo1.PNG", "ojoIA/trigo2.PNG", "ojoIA/trigo3.PNG","ojoIA/
 fresnoPaths = ["ojoIA/fresno1.PNG", "ojoIA/fresno2.PNG", "ojoIA/fresno3.PNG"]
 castanoPaths = ["ojoIA/casta1.PNG", "ojoIA/casta2.PNG", "ojoIA/casta3.PNG", "ojoIA/casta4.PNG" ]
 ortigaPaths = ["ojoIA/ortiga1.PNG", "ojoIA/ortiga2.PNG", "ojoIA/ortiga3.PNG", "ojoIA/ortiga4.PNG", "ojoIA/ortiga5.PNG"]
+salviaPaths = ["ojoIA/salvia1.PNG","ojoIA/salvia2.PNG"]
+nogalPaths = ["ojoIA/nogal1.PNG","ojoIA/nogal2.PNG", "ojoIA/nogal3.PNG"]
+
 hierroPaths = ["ojoIA/hierro1.PNG", "ojoIA/hierro2.PNG", "ojoIA/hierro3.PNG","ojoIA/hierro4.PNG", "ojoIA/hierro5.PNG"]
 
 
 confidenceLevel = 0.7
 screenshotsDir = "ojoIA"
-DIRECTIONS = [ 'right','right', 'up','up','right','up','up','right','down','right','right','up','right','up','up','left','up','right','up','left']
+DIRECTIONS = [ 'right','right', 'up','up','right','up','up','right','down','down','down','up','up','right','right','up','right','up','up','left','up','right','up','left']
 #1,23 TR
 #4,21
 CURRENT_DIRECTION_INDEX = 0
-REGION_TO_CAPTURE = (0, 25, 280, 220)
+REGION_TO_CAPTURE = (0, 65, 100 , 90-50)
+COMBAT_MODE_REGION = (0, 65, 100, 40)
 TOOLTIP_REGIONS = {
     'up': (960 - 50, 0, 100, 50),   
     'left': (0 + 300, 540 - 25, 100, 50),  
@@ -40,70 +46,148 @@ TOOLTIP_IMAGES = {
     'right': 'ojoIA/tooltip_image3.PNG',
 }
 
+map_data_statistics = {}
+
 if not os.path.exists(mapLocationDir):
     os.makedirs(mapLocationDir)
 
 if not os.path.exists(screenshotsDir):
     os.makedirs(screenshotsDir)
 
+def update_map_statistics(coordinates, zone_name):
+    """Agrega los datos del mapa al diccionario y los escribe en Excel."""
+    # Si la zona no está en el diccionario, la añade
+    if zone_name not in map_data_statistics:
+        map_data_statistics[zone_name] = {'coordinates': coordinates, 'count': 1}
+    else:
+        # Si la zona ya está, solo incrementa el contador
+        map_data_statistics[zone_name]['count'] += 1
+
+    # Convierte el diccionario a DataFrame de pandas para escribir en Excel
+    df = pd.DataFrame.from_dict(map_data_statistics, orient='index')
+
+    # Define la ruta del archivo Excel dentro del directorio mapLocationDir
+    excel_path = os.path.join(mapLocationDir, 'map_data_statistics.xlsx')
+    # Escribe el DataFrame en un archivo Excel
+    df.to_excel(excel_path, index_label='Zone')
+
+def is_exception_case(object_name, pos):
+    # Definimos las excepciones previsibles
+    exceptions = {
+        'fresno': {
+            'ignored_positions': [(1438, 822)],  # Lista de posiciones a ignorar
+        },
+        'trigo': {
+            'special_click_offsets': [(907, 588, 4, 0)],  # (x, y, offset_x, offset_y) donde x, y es la posición a buscar y (offset_x, offset_y) es cuánto mover el mouse antes de clickear
+        }
+    }
+
+    # Verificamos si el objeto actual tiene alguna excepción
+    if object_name in exceptions:
+        # Para el caso de fresno ignorado
+        if 'ignored_positions' in exceptions[object_name]:
+            for ignored_pos in exceptions[object_name]['ignored_positions']:
+                if pos.left == ignored_pos[0] and pos.top == ignored_pos[1]:
+                    print(f"[INFO] {object_name} encontrado en posición ignorada: {ignored_pos}. Ignorando...")
+                    return True  # Retorna True si es una excepción
+
+        # Para el caso de trigo que necesita un clic especial
+        if 'special_click_offsets' in exceptions[object_name]:
+            for special_click in exceptions[object_name]['special_click_offsets']:
+                if pos.left == special_click[0] and pos.top == special_click[1]:
+                    # Realiza el clic especial
+                    pg.moveTo(pos.left + special_click[2], pos.top + special_click[3])
+                    pg.click()
+                    print(f"[INFO] Clic especial para {object_name} realizado en {pos.left + special_click[2]}, {pos.top + special_click[3]}.")
+                    return True  # Retorna True si es una excepción
+
+    return False 
+
 def click_image(paths, object_name):
-    image_not_found_count = 0  # Contador para el número de imágenes no encontradas
+    image_not_found_count = 0
 
     for path in paths:
         try:
+            print(f"[BUSCANDO] {object_name}...")
             pos = pg.locateOnScreen(path, confidence=confidenceLevel)
+
+            # Verifica si la posición encontrada es una excepción
+            if pos and is_exception_case(object_name, pos):
+                continue  # Ignora esta posición y continúa con la siguiente imagen
+
             if pos:
-                pg.moveTo(pos[0] + imageOffset, pos[1] + imageOffset)
+                # Ajuste para salvia: mover un píxel más arriba antes de clickear
+                if object_name == 'salvia':
+                    click_x, click_y = pos.left + imageOffset, pos.top + imageOffset - 1
+                else:
+                    click_x, click_y = pos.left + imageOffset, pos.top + imageOffset
+
+                pg.moveTo(click_x, click_y)
                 pg.click()
-                print(f"[INFO] Se encontró {object_name} y se hizo clic en {pos}.")
+                print(f"[INFO] {object_name} encontrado y clickeado en {pos}.")
                 time.sleep(waitTime)
-                capture_screenshot(pos, object_name)
+                #capture_screenshot(pos, object_name)
                 return True
+
         except pg.ImageNotFoundException:
             image_not_found_count += 1
-            continue  # Continúa con la siguiente imagen
         except Exception as e:
-            print(f"[ERROR] Error al buscar {object_name} con el archivo {path}: {e}")
+            print(f"[ERROR] Error al buscar {object_name} con {path}: {e}")
 
     if image_not_found_count == len(paths):
-        print(f"[INFO] No se encontró {object_name} con ninguna de las imágenes proporcionadas.")
+        print(f"[INFO] {object_name} no encontrado con las imágenes proporcionadas.")
 
     return False
-
 
 def take_screenshot(region):
     return pg.screenshot(region=region)
 
 def resource_search_loop():
     while True:
-        resource_found = any(
-            click_image(paths, resource) for paths, resource in [
-                (trigoPaths, "trigo"),
-                (fresnoPaths, "fresno"),
-                (castanoPaths, "castaño"),
-                (ortigaPaths, "ortiga"),
-               # (hierroPaths, "hierro")
+        try:
+            if is_in_combat_mode():
+                print("[INFO] Modo combate detectado. Entrando en flujo de combate...")
+                while is_in_combat_mode():
+                    print("[INFO] En combate, esperando a que finalice...")
+                    time.sleep(5)  # Espera un tiempo antes de volver a chequear.
+                print("[INFO] Modo combate finalizado. Reanudando búsqueda de recursos.")
+                continue
+        except Exception as e:
+            print(f"[ERROR] Un error ocurrió mientras se verificaba el modo de combate: {e}")
+        print("[FASE] Comenzando búsqueda de recursos...")
+        
+        resource_found = False
+        for paths, resource in [(trigoPaths, "trigo"), (fresnoPaths, "fresno"), (castanoPaths, "castaño"),(nogalPaths, "nogal"), (ortigaPaths, "ortiga"),(salviaPaths, "salvia")]:
+            resource_found = click_image(paths, resource)
+            if resource_found:
+                break  # Si se encuentra un recurso, rompe el bucle y reinicia la búsqueda
 
-            ]
-        )
         if not resource_found:
-            print("No se encontraron recursos. Intentando cambiar de mapa.")
-            if change_map():
-                print("Cambio de mapa exitoso. Reanudando la búsqueda de recursos.")
+            print("[INFO] No se encontraron recursos. Intentando cambiar de mapa...")
+            change_map_result = change_map()
+            if change_map_result == True:
+                print("[INFO] Cambio de mapa exitoso.")
+            elif change_map_result == 'combat':
+                print("[INFO] Modo combate detectado durante el cambio de mapa. Pausando búsqueda de recursos.")
+                while is_in_combat_mode():
+                    print("[INFO] En combate, esperando a que finalice...")
+                    time.sleep(5)
+                print("[INFO] Modo combate finalizado. Reanudando búsqueda de recursos.")
             else:
-                print("Fallo al intentar cambiar de mapa.")
-                break
-
-# ... (resto del código antes de la función change_map)
+                print("[ERROR] Fallo al cambiar de mapa. Deteniendo la búsqueda.")
+                break  # Sale del bucle si falla el cambio de mapa.
 
 def change_map():
     global CURRENT_DIRECTION_INDEX
     try:
-        # Toma una captura de pantalla del área de interés y extrae el texto
+        # Toma una captura de pantalla del área de interés y extrae el texto para estadísticas
         screenshot_before = take_and_save_screenshot("before_map_change.png", REGION_TO_CAPTURE)
-        text_before = pytesseract.image_to_string(screenshot_before)
+        text_before = pytesseract.image_to_string(screenshot_before).strip()
         print("[INFO] Estado del mapa antes del cambio:")
-        print(f"       Texto extraído: {text_before.strip()}")
+        print(f"       Texto extraído para estadísticas: {text_before}")
+
+        # Actualiza las estadísticas antes del cambio de mapa
+        update_map_statistics("Before Change", text_before)
 
         # Intenta cambiar de mapa en una dirección
         direction = DIRECTIONS[CURRENT_DIRECTION_INDEX]
@@ -114,27 +198,37 @@ def change_map():
         pg.click()
         time.sleep(6)  # Tiempo de espera ajustable según la velocidad de cambio de mapa
 
-        # Toma otra captura de pantalla después del cambio de mapa y extrae el texto
-        # Captura y análisis del estado del mapa después del cambio
+        # Toma otra captura de pantalla después del cambio de mapa
         screenshot_after = take_and_save_screenshot("after_map_change.png", REGION_TO_CAPTURE)
-        text_after = pytesseract.image_to_string(screenshot_after)
+        text_after = pytesseract.image_to_string(screenshot_after).strip()
         print("[INFO] Estado del mapa después del cambio:")
-        print(f"       Texto extraído: {text_after.strip()}")
+        print(f"       Texto extraído para estadísticas: {text_after}")
 
-        # Comparación de textos para confirmar el cambio de mapa
-        if text_before != text_after:
-            print("[SUCCESS] El cambio de mapa parece haber sido exitoso.")
+        # Actualiza las estadísticas después del cambio de mapa
+        update_map_statistics("After Change", text_after)
+
+        # Utiliza ImageChops para verificar si hay diferencia entre las imágenes
+        if ImageChops.difference(screenshot_before, screenshot_after).getbbox() is None:
+            print("[WARNING] No se detectaron cambios en las coordenadas del mapa.")
+            # Llama a is_in_combat_mode para comprobar si estamos en modo combate
+            if is_in_combat_mode():
+                print("[INFO] Entrando en flujo de combate...")
+                # Aquí iría la lógica para manejar el modo combate
+                # break # Si deseas salir del bucle en este punto
+                return 'combat'  # Puedes retornar un valor específico para indicar el modo combate
+            else:
+                print("[INFO] No se detectó el modo combate, puede ser un fallo al cambiar de mapa.")
+                return False
         else:
-            print("[WARNING] No se detectaron cambios significativos en la posición del mapa.")
-
-        return text_before != text_after  # Retorna True si hay un cambio, False si no
+            print("[SUCCESS] El cambio de mapa parece haber sido exitoso.")
+            return True
 
     except Exception as e:
         print(f"[ERROR] Excepción capturada durante el cambio de mapa: {e}")
         return False
 
-
 # ... (resto del código posterior a la función change_map)
+
 
 def capture_screenshot(pos, object_name, offset=25, size=100, directory="ojoIA"):
     if pos is None:
@@ -164,6 +258,32 @@ def screenshots_are_different(img1, img2):
     if diff.getbbox():
         return True
     else:
+        return False
+
+def extract_information(text):
+    """Extrae la información cruda sin usar regex."""
+    return text.strip()
+
+def is_in_combat_mode():
+    try:
+        # Definir la región donde se busca el indicador de combate.
+        COMBAT_INDICATOR_REGION = (1310, 930, 165 , 75)  # x, y, ancho, alto
+        
+        # Ruta a la imagen del indicador de combate.
+        combat_indicator_image_path = 'ojoIA/combat_indicator.PNG'
+
+        # Verificar si el indicador de combate está presente en la región definida.
+        combat_indicator = pg.locateOnScreen(combat_indicator_image_path, region=COMBAT_INDICATOR_REGION, confidence=0.4)
+
+        if combat_indicator:
+            print("[INFO] Modo combate detectado.")
+            return True
+        else:
+            print("[INFO] No se detectó el modo combate.")
+            return False
+    except Exception as e:
+        print(f"[ERROR] Un error ocurrió mientras se verificaba el modo de combate: {e}")
+        # Dependiendo de tu flujo, podrías querer retornar False o manejar de otra manera
         return False
 
 
